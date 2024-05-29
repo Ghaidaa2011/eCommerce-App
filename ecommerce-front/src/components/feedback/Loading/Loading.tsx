5b7f77e2ad8fa2a8f13ca451dc59918867020cfc
Ghaidaa <117023274+Ghaidaa2011@users.noreply.github.com>
2024-05-29 23:01:19 +0300

    first commit

--- ecommerce-front/src/components/feedback/Loading/Loading.tsx
+++ ecommerce-front/src/components/feedback/Loading/Loading.tsx
@@ -1,14 +1,24 @@
 import { TLoading } from "@types";
-
+import CategorySkeleton from "../skeletons/CategorySkeleton/CategorySkeleton";
+import CartSkeleton from "../skeletons/CartSkeleton/CartSkeleton";
+import ProductSkeleton from "../skeletons/ProductSkeleton/ProductSkeleton";
+//dynamic component
+const skeletonsTypes = {
+  category: CategorySkeleton,
+  cart: CartSkeleton,
+  product: ProductSkeleton,
+};
 type LoadingProps = {
   status: TLoading;
   error: null | string;
   children: React.ReactNode;
+  type: keyof typeof skeletonsTypes;
 };
 
-const Loading = ({ status, error, children }: LoadingProps) => {
+const Loading = ({ status, error, children, type }: LoadingProps) => {
+  const Component = skeletonsTypes[type];
   if (status === "pending") {
-    return <div>loading please wait</div>;
+    return <Component />;
   }
   if (status === "failed") {
     return <div>{error}</div>;
--- ecommerce-front/src/pages/Wishlist.tsx
+++ ecommerce-front/src/pages/Wishlist.tsx
@@ -9,7 +9,7 @@ const Wishlist = () => {
   return (
     <>
       <Heading title="Your Wishlist" />
-      <Loading status={loading} error={error}>
+      <Loading status={loading} error={error} type="product">
         <GridList<TProduct>
           records={records}
           renderItem={(record) => <Product {...record} />}
