bba352800fbced0d37c43aba43776068cb1be18e
Ghaidaa <117023274+Ghaidaa2011@users.noreply.github.com>
2024-06-14 15:37:58 +0300

    first commit

--- ecommerce-front/src/components/common/Header/Header.tsx
+++ ecommerce-front/src/components/common/Header/Header.tsx
@@ -1,11 +1,14 @@
+import { useAppDispatch, useAppSelector } from "@store/hooks";
+import { authLogout } from "@store/auth/authSlice";
 import { NavLink } from "react-router-dom";
-import { Badge, Navbar, Nav, Container } from "react-bootstrap";
-
+import { Badge, Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import styles from "./styles.module.css";
 import HeaderLeftBar from "./HeaderLeftBar/HeaderLeftBar";
 const { headerContainer, headerLogo } = styles;
 
 const Header = () => {
+  const dispatch = useAppDispatch();
+  const { user, accessToken } = useAppSelector((state) => state.auth);
   return (
     <header>
       <div className={headerContainer}>
@@ -36,12 +39,34 @@ const Header = () => {
               </Nav.Link>
             </Nav>
             <Nav>
-              <Nav.Link as={NavLink} to="login">
-                Login
-              </Nav.Link>
-              <Nav.Link as={NavLink} to="register">
-                Register
-              </Nav.Link>
+              {!accessToken ? (
+                <>
+                  <Nav.Link as={NavLink} to="login">
+                    Login
+                  </Nav.Link>
+                  <Nav.Link as={NavLink} to="register">
+                    Register
+                  </Nav.Link>
+                </>
+              ) : (
+                <NavDropdown
+                  title={`Welcome ${user?.firstName} ${user?.lastName}`!}
+                  id="basic-nav-dropdown"
+                >
+                  <NavDropdown.Item as={NavLink} to="profile">
+                    Profile
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>Orders</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item
+                    as={NavLink}
+                    to="/"
+                    onClick={() => dispatch(authLogout())}
+                  >
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
--- ecommerce-front/src/pages/Login.tsx
+++ ecommerce-front/src/pages/Login.tsx
@@ -1,5 +1,82 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "@store/hooks";
+import { actAuthLogin, resetUI } from "@store/auth/authSlice";
+import { useSearchParams, useNavigate } from "react-router-dom";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { signInSchema, signInType } from "@validations/signInSchema";
+import { Heading } from "@components/common";
+import { Input } from "@components/form";
+import { Form, Button, Row, Col, Alert, Spinner } from "react-bootstrap";
+
 const Login = () => {
-  return <div>Login</div>;
+  const dispatch = useAppDispatch();
+  const { loading, error } = useAppSelector((state) => state.auth);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<signInType>({
+    mode: "onBlur",
+    resolver: zodResolver(signInSchema),
+  });
+  const submitForm: SubmitHandler<signInType> = (data) => {
+    if (searchParams.get("message")) {
+      setSearchParams("");
+    }
+    dispatch(actAuthLogin(data))
+      .unwrap()
+      .then(() => navigate("/"));
+  };
+  useEffect(() => {
+    return () => {
+      dispatch(resetUI());
+    };
+  }, [dispatch]);
+  return (
+    <>
+      <Heading title="User Login" />
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          {searchParams.get("message") === "account_created" && (
+            <Alert variant="success">
+              Your account successfully created, please log in.
+            </Alert>
+          )}
+          <Form onSubmit={handleSubmit(submitForm)}>
+            <Input
+              label="Email"
+              name="email"
+              register={register}
+              error={errors.email?.message}
+            />
+            <Input
+              type="password"
+              label="Password"
+              name="password"
+              register={register}
+              error={errors.password?.message}
+            />
+            <Button variant="info" type="submit" style={{ color: "white" }}>
+              {loading === "pending" ? (
+                <>
+                  <Spinner animation="border" size="sm"></Spinner> Loading...
+                </>
+              ) : (
+                "Submit"
+              )}
+            </Button>
+            {error && (
+              <p style={{ color: "#DC3545", marginTop: "10px" }}>{error}</p>
+            )}
+          </Form>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default Login;
